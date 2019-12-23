16246ec591aa30fc8e3ada7a9b218af191799811
howardlau <howardlau1999@hotmail.com>
2019-12-23 10:39:57 +0800

    add: 添加简单的IP收集

---
+++ collectip.php
@@ -0,0 +1,27 @@
+<?php
+require "include/bittorrent.php";
+dbconn();
+
+stdhead();
+
+if ($_GET['campus'] == 'east') {
+    $campus = '广州校区东校园';
+} else if ($_GET['campus'] == 'north') {
+    $campus = '广州校区北校园';
+} else if ($_GET['campus'] == 'south') {
+    $campus = '广州校区南校园';
+} else if ($_GET['campus'] == 'zhuhai') {
+    $campus = '珠海校区';
+} else {
+    $campus = '未知校区';
+}
+
+$ip = getip();
+
+if (validateIPv6($ip)) {
+    $ipv6 = $ip;
+} else {
+    $ipv6 = '未知 IP';
+}
+
+stdmsg("感谢您的参与", "已收集您的 IP 信息【" . $ipv6 . "】<br>对应校区【" . $campus . "】。<br><a href='/index.php'>点击这里返回主页</a>");
